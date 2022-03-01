5e62b89c2ebae62ea77f897cad63dd08fa53710c
SylarWu <702005971@qq.com>
2022-03-01 10:59:39 +0800

    【每日一题】 leetcode 6，简单的Z字模拟

--- java/src/main/java/tech/sylardaemon/Solution6.java
+++ java/src/main/java/tech/sylardaemon/Solution6.java
@@ -2,6 +2,46 @@ package tech.sylardaemon;
 
 public class Solution6 {
     public String convert(String s, int numRows) {
+        if (s == null || s.length() == 1 || numRows == 1){
+            return s;
+        }
+        StringBuilder result = new StringBuilder();
+        int n = s.length();
+        for (int i = 0; i < numRows; ++i){
+            int cur = i;
+            int step = (numRows - 1 - i) * 2;
+            while (cur < n){
+                if (step != 0){
+                    result.append(s.charAt(cur));
+                }
+                cur += step;
+                step = (numRows - 1) * 2 - step;
+            }
+        }
+        return result.toString();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private String originMethod(String s, int numRows){
         if (numRows == 1 || s == null || s.equals("")){
             return s;
         }
